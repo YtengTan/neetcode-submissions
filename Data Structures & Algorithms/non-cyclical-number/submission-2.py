ab417b176589e33b23e3ed8bdaf89deb959da2ff
Yaoteng Tan <93806940+YtengTan@users.noreply.github.com>
2026-05-03 16:04:36 -0700

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen_number = []
+
+        n_str = str(n)
+        number = sum([int(digit)**2 for digit in n_str])
+        
+        
+        while number not in seen_number:
+            seen_number.append(number)
+            n_str = str(number)
+            number = sum([int(digit)**2 for digit in n_str])
+        
+        if number == 1:
+            return True
+        else:
+            return False
